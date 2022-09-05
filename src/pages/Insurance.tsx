df2a4b7bab4dd22c9eea32f81f79b97f8722180f
aidannairn <aidan.nairn95@gmail.com>
2022-09-05 17:53:34 +1200

    feat: Click a step to update the current active step.

--- src/pages/Insurance.tsx
+++ src/pages/Insurance.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Header } from '../components/Header/Header'
 import './insurance.css'
 
@@ -9,36 +11,43 @@ interface StepInterface {
 
 interface StepOptionProps {
   step: StepInterface,
+  setIsActive: React.Dispatch<React.SetStateAction<string>>
 }
 
-const FormStepOption: React.FC<StepOptionProps> = ({ step }) => {
+const FormStepOption: React.FC<StepOptionProps> = ({ step, setIsActive }) => {
+
+  const handleOptionClick: React.MouseEventHandler<HTMLDivElement> = () => setIsActive(step.name)
 
   return (
     <div id={`form-step-option`} className={
       `${step.isActive ? 'active' : null}
       ${step.isComplete && !step.isActive ? 'complete' : null}`
-    }>
+    }
+      onClick={handleOptionClick}
+    >
       <h2>{step.name}</h2>
-      {step.isComplete && <img src="images/tick-box.png" alt="Completed checkbox" />}
+      {(step.isComplete && !step.isActive) && <img src="images/tick-box.png" alt="Completed checkbox" />}
     </div>
   )
 }
 
 const Insurance: React.FC = () => {
+  const [isActive, setIsActive] = useState<string>('drivers')
+
   const steps: StepInterface[] = [
     {
       name: 'drivers',
-      isActive: false,
-      isComplete: true
+      isActive: isActive === 'drivers',
+      isComplete: false
     },
     {
       name: 'cars',
-      isActive: true,
+      isActive: isActive === 'cars',
       isComplete: false
     },
     {
       name: 'polices & extras',
-      isActive: false,
+      isActive: isActive === 'polices & extras',
       isComplete: false
     }
   ]
@@ -49,7 +58,7 @@ const Insurance: React.FC = () => {
       <div id='insurance-container'>
         <div id="form-step-options">
           {steps.map((step: StepInterface, i: number) => {
-            return <FormStepOption key={i} step={step} />
+            return <FormStepOption key={i} step={step} setIsActive={setIsActive} />
           })}
         </div>
       </div>
