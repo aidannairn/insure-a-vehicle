86f402b47161b64cc88934e1b612e53131b668c4
Jay Kang <qhdl24@gmail.com>
2022-09-05 20:46:12 +1200

    setupTest fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { Home } from '../src/pages/Home';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Home } from "../src/pages/Home";
 
-test('renders turners insurance page', () => {
+test("renders turners insurance page", () => {
   render(<Home />);
   const linkElement = screen.getByText(/Discover our insurances/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
